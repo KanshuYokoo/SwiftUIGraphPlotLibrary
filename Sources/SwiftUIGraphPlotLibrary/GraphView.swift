4c2564fe298349a6a180a9ebd7a9d05870534fd9
Kanshu Yokoo <kanshu.yokoo@gmail.com>
2020-04-18 01:02:12 +0300

    improvement; main GraphPlot

--- Sources/SwiftUIGraphPlotLibrary/GraphView.swift
+++ Sources/SwiftUIGraphPlotLibrary/GraphView.swift
@@ -11,18 +11,24 @@ import SwiftUI
 public struct GraphView: View {
     let grapFrameSize:CGSize
     let dataSet:[PlotData]
-    let viewSet:[GraphPlotView]?
-    public let frameView:FrameView
-    public let plotView:PlotView
     
-    public init(dataSet:[PlotData], plotViews:[GraphPlotView]? = nil, frameSize:CGSize) {
+    public let frameView:FrameView
+    public var plotView:PlotView
+    
+    public init(dataSet:[PlotData], plotTypes:[GraphPlot], frameSize:CGSize) {
         self.dataSet = dataSet
         self.grapFrameSize = frameSize
-        self.viewSet = plotViews
-        
-        self.plotView = PlotView(dataSet: dataSet)
         self.frameView = FrameView()
         
+        self.plotView = PlotView(dataSet: dataSet, plotTypes: plotTypes)
+    }
+    
+    public init(dataSet:[PlotData], plotType:GraphPlot, frameSize:CGSize) {
+        self.dataSet = dataSet
+        self.grapFrameSize = frameSize
+        self.frameView = FrameView()
+               
+        self.plotView = PlotView(dataSet: dataSet, plotTypes: [plotType])
     }
 
     var graphWidth:CGFloat {
@@ -72,37 +78,100 @@ public struct FrameView:View {
 
 public struct PlotView: View {
     let dataSet:[PlotData]
-    let viewSet:[GraphPlotView]?
+    let plotSet:[GraphPlot]
     
-    init(dataSet:[PlotData], plotViews:[GraphPlotView]? = nil) {
+    public init(dataSet:[PlotData], plotTypes:[GraphPlot]) {
         self.dataSet = dataSet
-        self.viewSet = plotViews
+        self.plotSet = plotTypes
+    }
+    
+    func getGraphPlotView(from plot:GraphPlot, geometryProxy:GeometryProxy) -> GraphPlotView {
+        return GraphPlotView(geometryProxy: geometryProxy, type: plot.plotType, dataSet: plot.dataSet ?? self.dataSet, color: plot.color, opacity: plot.opacity, circleRadius: plot.circleRadius, hueDegree: plot.hueDegree, circleRadiusFunc: plot.circleRadiusFunc, colorFunc: plot.colorFunc, hueDegreeFunc: plot.hueDegreeFunc)
     }
     public var body:some View {
+        
         GeometryReader {proxy in
             
         return ZStack{
-            //GraphFrameView(geometryProxy:proxy)
-            
+            ForEach(0..<self.plotSet.count) { index in
+                self.getGraphPlotView(from: self.plotSet[index], geometryProxy: proxy)
+            }
+
+            /*
             GraphPlotView(type:.linePlot, geometryProxy: proxy, dataSet: self.dataSet)
-            
             GraphPlotView(type:.circlePlot, geometryProxy: proxy, dataSet: self.dataSet)
-            
             GraphPlotView(type:.verticalBar, geometryProxy: proxy, dataSet: self.dataSet)
+            */
+            
             }
         }
     }
     
-
 }
+
+public struct GraphPlot {
+    
+    let plotType:PlotType
+    var color:Color?
+    var dataSet:[PlotData]?
+    
+    var opacity:Double?
+    var circleRadius:CGFloat?
+    var hueDegree:Double?
+    
+    var circleRadiusFunc: RadiusFunc?
+    var colorFunc: ColorFunc?
+    var hueDegreeFunc: HueDegreeFunc?
+    
+    public init(type:PlotType,  dataSet:[PlotData]? = nil, color:Color? = nil, opacity:Double? = nil, circleRadius:CGFloat? = nil,  hueDegree:Double? = nil) {
+        
+        self.plotType = type
+        self.dataSet = dataSet
+        self.opacity = opacity
+        self.circleRadius = circleRadius
+        self.hueDegree = hueDegree
+    }
+
+    public func setHueDegreeFunc(function:@escaping HueDegreeFunc) ->  GraphPlot{
+        var graphPlot = self
+        graphPlot.hueDegreeFunc = function
+        return graphPlot
+    }
+    
+    public func setColorFunc(function:@escaping ColorFunc) ->  GraphPlot{
+        var graphPlot = self
+        graphPlot.colorFunc = function
+        return graphPlot
+    }
+    
+    public func setCircleRadiusFunc(function:@escaping RadiusFunc) ->  GraphPlot{
+        var graphPlot = self
+        graphPlot.circleRadiusFunc = function
+        return graphPlot
+    }
+    
+    public func setCircle(color:Color? = nil, radius:CGFloat? = nil, radiusFunc:RadiusFunc? = nil) -> GraphPlot {
+        var graphPlot = self
+        if let color = color {
+            graphPlot.color = color
+        }
+        if let radius = radius {
+            graphPlot.circleRadius = radius
+        }
+        graphPlot.circleRadiusFunc = radiusFunc
+        return graphPlot
+    }
+    
+}
+
 
 struct GraphView_Previews: PreviewProvider {
     
     static var previews: some View {
         let plotset:[PlotData] = [PlotData(x: 0,y: 0),PlotData(x: 1.0,y: 2.0),PlotData(x: 2.0,y: 3.0),PlotData(x: 3.0,y: 2.0),PlotData(x: 4.0,y: 7.0),PlotData(x: 40.0,y: 20.0)]
+        let plotType = GraphPlot(type: .linePlot)
         
-        
-        return GraphView(dataSet: plotset, frameSize: CGSize(width: 300, height: 200) ).frame(width: 350, height: 250, alignment: .center)
+        return GraphView(dataSet: plotset,plotType: plotType, frameSize: CGSize(width: 300, height: 200) ).frame(width: 350, height: 250, alignment: .center)
     }
 }
 
