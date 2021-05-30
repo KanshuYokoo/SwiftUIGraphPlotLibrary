6d017dc5637f7cd77b6a74012fd77f9e8c2f2875
Kanshu Yokoo <kanshu.yokoo@gmail.com>
2021-05-30 10:02:03 +0300

    update Package.swift: type: .dynamicc

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftUIGraphPlotLibrary",
+            type: .dynamic,
             targets: ["SwiftUIGraphPlotLibrary"]),
     ],
     dependencies: [
