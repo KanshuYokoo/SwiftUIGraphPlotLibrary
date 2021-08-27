70a29e68f3e2aa17cdc5a83788f58b601ed49c57
Kanshu Yokoo <kanshu.yokoo@gmail.com>
2021-08-27 06:21:04 +0300

    start adding xtest

--- Tests/SwiftUIGraphPlotLibraryTests/SwiftUIGraphPlotLibraryTests.swift
+++ Tests/SwiftUIGraphPlotLibraryTests/SwiftUIGraphPlotLibraryTests.swift
@@ -2,12 +2,18 @@ import XCTest
 @testable import SwiftUIGraphPlotLibrary
 
 final class SwiftUIGraphPlotLibraryTests: XCTestCase {
+
+    override class func setUp() {
+        let xarryInt5 :[Int] = [1,2,3,4,5]
+        let yarryInt5 :[Int] = [1,4,9.16,5]
+    }
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-
     }
+    
+    
 
     static var allTests = [
         ("testExample", testExample),
